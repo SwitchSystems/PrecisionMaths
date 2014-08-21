05433e2a4f53b61aece58a5805700bfa94848f47
Jon Day <jon@switchsystems.co.uk>
2014-08-21 11:21:52 +0100

    PMATH-1 added build.xml and test suite

---
+++ test/Bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+namespace PrecisionMathsTest;
+
+putenv('APPLICATION_ENV=test');
+
+error_reporting(-1);
+
+class Bootstrap
+{
+	protected static $serviceManager;
+	protected static $config;
+	protected static $bootstrap;
+
+	public static function init()
+	{
+        
+	}
+}
+
+Bootstrap::init();
