b122d7d77bd58ee264b82c66f84e0ec7a732421d
Jon Day <jon@switchsystems.co.uk>
2014-08-22 11:52:52 +0100

    PMATH-3 adding a calc in minutes to DecimalTime Class

--- src/PrecisionMaths/Utility/DecimalTime.php
+++ src/PrecisionMaths/Utility/DecimalTime.php
@@ -5,6 +5,11 @@ use DateTime;
 use InvalidArgumentException;
 use PrecisionMaths\Number;
 
+/**
+ * Utility for precise Decimal Time Calculations
+ * @TODO: Implement Month and Year - currently these will just be ignored!
+ * as we don't need this functionality as of yet
+ */
 class DecimalTime
 {
     /**
@@ -25,36 +30,25 @@ class DecimalTime
     /**
      * @var string
      */
-    const DAYS = 'days';
-    
-    /**
-     * @var string
-     */
-    const HOURS = 'hours';
-    
-    /**
-     * @var string
-     */
-    const MINUTES = 'minutes';
-    
-    /**
-     * @var string
-     */
-    const SECONDS = 'seconds';
-    
+    const MINUTES_IN_DAY = '1440';
     
 	public function dateRangeAsDays(DateTime $start, DateTime $end)
 	{
 		
 	}
 
+	/**
+	 * 
+	 * @param DateTime $start
+	 * @param DateTime $end
+	 * @return PrecisionMaths\Number
+	 */
 	public function dateRangeAsHours(DateTime $start, DateTime $end)
 	{
 	    $dateInterval = $this->calculateDateDiff($start, $end);
 	    
-	    // Calculate the hours in a day and add to $hours
-	    $hoursFromDays = (new Number($dateInterval->d))->mul(static::HOURS_IN_DAY);
-	    $hours = $hoursFromDays;
+	    // Calculate the hours in a day and assign to $hours
+	    $hours = (new Number($dateInterval->d))->mul(static::HOURS_IN_DAY);
 
 	    // Add the hours
 	    $hours = $hours->add($dateInterval->h);
@@ -73,58 +67,21 @@ class DecimalTime
 	
 	public function dateRangeAsMinutes(DateTime $start, DateTime $end)
 	{
+	    $dateInterval = $this->calculateDateDiff($start, $end);
+	    
+	    // Calculate the minutes from days and assign to $minutes
+	    $minutes = (new Number($dateInterval->d))->mul(static::MINUTES_IN_DAY);
 
+	    // Calculate minutes from hours and add to $minutes
+	    $minutesFromHours = (new Number($dateInterval->h))->mul(static::MINUTES_IN_HOUR);
+	    $minutes->add($minutesFromHours);
 	}
 	
 	public function dateRangeAsSeconds(DateTime $start, DateTime $end)
 	{
 	
 	}
-	
-	protected function dateRangeAs(DateTime $start, DateTime $end, $unit)
-	{
-	    $dateInterval = $this->calculateDateDiff($start, $end);
-	    
-        switch ($unit) {
-        	case static::DAYS:
-        	    $dateRange = null;
-        	    return $dateRange;
-        	    break;
-        	case static::HOURS:
-        	    //Calculate the hours
-        	    $hoursFromDays = (new Number($dateInterval->d))->mul(static::HOURS_IN_DAY);
-        	    $hours = $hoursFromDays;
-        	    
-        	    $hours = $hours->add($dateInterval->h);
-        	    
-        	    $hoursFromMinutes = (new Number($dateInterval->i))->div(static::MINUTES_IN_HOUR);
-        	    $hours = $hours->add($hoursFromMinutes);
-        	    
-        	    $secondsInMinutes = (new Number($dateInterval->i))->div(static::SECONDS_IN_MINUTE);
-        	    $hoursFromSeconds = $secondsInMinutes->div(static::MINUTES_IN_HOUR);
-        	    $hours = $hours->add($hoursFromSeconds);
-        	    
-        	    $dateRange = null;
-        	    return $dateRange;
-        	    break;
-        	case static::MINUTES:
-        	    $dateRange = null;
-        	    return $dateRange;
-        	    break;
-        	case static::SECONDS:
-        	    $dateRange = null;
-        	    return $dateRange;
-        	    break;
-        	default:
-        	    throw new InvalidArgumentException(sprintf('Invalid $unit (%s) provided to PrecisionMaths\Utility\DecimalTime::dateRangeAs method', $unit));
-        }
-	}
-	
-	protected function isValidUnit($unit)
-	{
-	    return in_array(strtolower($unit), static::$validUnitsList);
-	}
-	
+
 	protected function calculateDateDiff(DateTime $start, DateTime $end)
 	{
 	    $dateInterval = $start->diff($end);
